ff08d556e587999dccbdd24652fa0af3093d444e
Naftali Berger <bn26665@gmail.com>
2025-08-07 12:28:49 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`server is running on port ${process.env.PORT || 3000}`);
+});
